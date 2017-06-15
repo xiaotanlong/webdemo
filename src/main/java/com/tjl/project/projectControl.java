28ac9355a345629a994376d4a4016571d36de09c
xiaotanlong <xiaotantjl@163.com>
2017-06-15 16:49:40 +0800

    sss

---
+++ src/main/java/com/tjl/project/projectControl.java
@@ -0,0 +1,11 @@
+package com.tjl.project;
+
+public class projectControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("uuuu");
+	}
+
+}
